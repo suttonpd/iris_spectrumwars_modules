70515c7288111c084617abf0f2451080c6af4f12
Paul Sutton <suttonpd@gmail.com>
2013-08-29 13:30:34 +0100

    bugfix

--- scripts/setupdirectlink.sh
+++ scripts/setupdirectlink.sh
@@ -9,7 +9,7 @@ sudo ifconfig $1 $2 netmask 255.255.255.0
 echo Interface up
 
 #Add a route to direct all usrp traffic to the correct interface
-sudo route add -net 192.168.2.0 netmask 255.255.255.0 gw 192.168.2.2 dev $1
+sudo route add -net 192.168.2.0 netmask 255.255.255.0 gw 192.168.2.1 dev $1
 echo Route added
 
 
